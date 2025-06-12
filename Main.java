936b5b3887381e98634ccf52766d9409ab6d2b9c
aizhekva <aishazhekenova@gmail.com>
2025-06-12 10:49:50 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        // ООП классы и объекты
+        //Car bmw = new Car(250.5f, 2500, "white", new byte[] {0, 0, 0});
+
+
+        //Truck truck = new Truck(5600, new byte[] {100, 0, 100}, true);
+
+        //truck.setLoaded(true);
+       //truck.getLoaded();
+
+    }
+}
+
+
+

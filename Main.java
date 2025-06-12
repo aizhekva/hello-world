f7305886262dc041281d6586028d60e3d5442bde
aizhekva <aishazhekenova@gmail.com>
2025-06-12 12:01:23 +0600

    Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Main{
-    static void main(String[] args){
+    public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
         System.out.print("Введите первое число: ");
         double num1 = scan.nextDouble();
